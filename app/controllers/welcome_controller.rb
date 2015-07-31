370301516b201f011630c41d8533f03a25d289eb
prathamalag1994 <prathamalag@berkeley.edu>
2015-07-31 16:21:32 -0700

    setting up the viewer

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,11 +2,13 @@ class WelcomeController < ApplicationController
   def index
   	key = 'y9mJIhH1eO8PZLCAVHS7qlv1EYveJqLi'
   	secret = 'WWPE4atWHynFb8yH'
-  	name = 'sfndsskdssn'   #the name you want to give your bucket
-  	policy = 'transient'
+  	name = 'sfndsskdssn'		#the name you want to give your bucket
+  	policy = 'transient'		#the retention policy you want to register your bucket with
 
-  	token = Adn_Viewer.token(key, secret)["access_token"]
-  	key =  Adn_Viewer.create_bucket(token, name, policy)["key"]
-  	
+  	supported_formats = Adn_Viewer.supported_formats(token)		#fills the variable supported_formats with all formats that the viewer supports (in json format)
+  	token = Adn_Viewer.token(key, secret)["access_token"]		#gives you an access token
+  	Adn_Viewer.create_bucket(token, name, policy)				#creates the bucket
+  	name = Adn_Viewer.check_bucket(token, name)["key"]			#checks status and returns bucket name	
+
   end
 end
