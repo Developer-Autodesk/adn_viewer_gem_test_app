d1c260ffda9357ee2935447dea93d3d1ce703d5f
prathamalag1994 <prathamalag@berkeley.edu>
2015-08-02 03:56:10 -0700

    fixing uploading

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,6 +10,7 @@ class WelcomeController < ApplicationController
    	supported_formats = Adn_Viewer.supported_formats(token)		#fills the variable supported_formats with all formats that the viewer supports (in json format) 	
   	Adn_Viewer.create_bucket(token, name, policy)				#creates the bucket
   	name =  Adn_Viewer.check_bucket(token, name)["key"]			#checks status and returns bucket name
+
   	#puts Adn_Viewer.upload_file(token, name, file)				#upload the file you want to view
   	#puts JSON.parse(CurbFu.get({:host => 'developer.api.autodesk.com', :path => '/oss/v1/buckets/' + name + '/objects/' + file, :protocol => "https", :headers => { "Authorization" => "Bearer " + token, "Content-Type" => "application/octet-stream" , "Content-Length" => 308331 }, "upload-file" => file }).body)
 	#bound = "AaB03xZZZZZZ11322321111XSDW"
@@ -23,11 +24,11 @@ class WelcomeController < ApplicationController
 	#request["authorization"] = 'Bearer ' + token
 	#request.add_field('session', bound)
 	#puts http.request(request).read_body
-	urn = "urn:adsk.objects:os.object:teststage/Test.dwg"
-	urn = Base64.encode64(urn) 				#encodes the urn to allow translation
+	
+	urn = 'urn:adsk.objects:os.object:teststage/Test.dwg'
+	urn = Base64.urlsafe_encode64(urn)				#encodes the urn to allow translation	
 	Adn_Viewer.register(token, urn)			#registers for translation
 	gon.token = token
 	gon.urn = urn
-
   end
 end
