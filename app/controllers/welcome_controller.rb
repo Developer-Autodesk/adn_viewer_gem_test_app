07a0dd4be24de3e0617ab06dc7df98f70b6afead
prathamalag1994 <prathamalag@berkeley.edu>
2015-08-02 00:53:08 -0700

    working on the uploading

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,17 @@ class WelcomeController < ApplicationController
   def index
   	key = 'y9mJIhH1eO8PZLCAVHS7qlv1EYveJqLi'
   	secret = 'WWPE4atWHynFb8yH'
-  	name = 'sfndsskdssn'		#the name you want to give your bucket
+  	name = 'teststage'			#the name you want to give your bucket
   	policy = 'transient'		#the retention policy you want to register your bucket with
+  	file = 'Test.dwg'
 
-  	supported_formats = Adn_Viewer.supported_formats(token)		#fills the variable supported_formats with all formats that the viewer supports (in json format)
   	token = Adn_Viewer.token(key, secret)["access_token"]		#gives you an access token
+   	supported_formats = Adn_Viewer.supported_formats(token)		#fills the variable supported_formats with all formats that the viewer supports (in json format) 	
   	Adn_Viewer.create_bucket(token, name, policy)				#creates the bucket
-  	name = Adn_Viewer.check_bucket(token, name)["key"]			#checks status and returns bucket name	
+  	name =  Adn_Viewer.check_bucket(token, name)["key"]			#checks status and returns bucket name
+  	#gon.token = Adn_Viewer.upload_file(token, name, file)["objects"]["id"]		#upload the file you want to view
+
+
 
   end
 end
