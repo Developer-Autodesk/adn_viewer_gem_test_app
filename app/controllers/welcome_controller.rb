3ad2d7bd07689084e4ac4dae121e9f6616a9c4cb
prathamalag1994 <prathamalag@berkeley.edu>
2015-08-02 03:11:45 -0700

    done except for uploading

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,9 +10,24 @@ class WelcomeController < ApplicationController
    	supported_formats = Adn_Viewer.supported_formats(token)		#fills the variable supported_formats with all formats that the viewer supports (in json format) 	
   	Adn_Viewer.create_bucket(token, name, policy)				#creates the bucket
   	name =  Adn_Viewer.check_bucket(token, name)["key"]			#checks status and returns bucket name
-  	#gon.token = Adn_Viewer.upload_file(token, name, file)["objects"]["id"]		#upload the file you want to view
-
-
+  	#puts Adn_Viewer.upload_file(token, name, file)				#upload the file you want to view
+  	#puts JSON.parse(CurbFu.get({:host => 'developer.api.autodesk.com', :path => '/oss/v1/buckets/' + name + '/objects/' + file, :protocol => "https", :headers => { "Authorization" => "Bearer " + token, "Content-Type" => "application/octet-stream" , "Content-Length" => 308331 }, "upload-file" => file }).body)
+	#bound = "AaB03xZZZZZZ11322321111XSDW"
+	#uri = URI("https://developer.api.autodesk.com/oss/v1/buckets/" + name + "/objects/" + file)
+	#http = Net::HTTP.new(uri.host, uri.port)
+	#http.use_ssl = true
+	#request = Net::HTTP::Put.new(uri)
+	#request.body_stream=File.open(file)
+	#request["content-type"] = 'application/octet-stream'
+	#request["Content-Length"] = 308331
+	#request["authorization"] = 'Bearer ' + token
+	#request.add_field('session', bound)
+	#puts http.request(request).read_body
+	urn = "urn:adsk.objects:os.object:teststage/Test.dwg"
+	urn = Base64.encode64(urn) 				#encodes the urn to allow translation
+	Adn_Viewer.register(token, urn)			#registers for translation
+	gon.token = token
+	gon.urn = urn
 
   end
 end
